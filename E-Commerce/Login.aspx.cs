ca7fe46c8c2d83f59a729cd57348aafc6557ee69
Giuseppe Cifarelli <cifarelli1995@gmail.com>
2023-09-18 12:53:25 +0200

    Added Login

--- E-Commerce/Login.aspx.cs
+++ E-Commerce/Login.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,7 +13,20 @@ namespace E_Commerce
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            errorMessage.Visible = false;
+        }
 
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            string user = username.Text;
+            string psw = password.Text;
+            if (user == ConfigurationManager.AppSettings["user"]
+                && psw == ConfigurationManager.AppSettings["psw"])
+            {
+                FormsAuthentication.SetAuthCookie(username.Text, false);
+                Response.Redirect(FormsAuthentication.DefaultUrl);
+            }
+            else errorMessage.Visible = true;
         }
     }
 }
