be570e0b89170dd9aa5afbe1d8c5b1beeb5ffdd1
NinoTraverso <njt98git@proton.me>
2023-09-18 11:40:06 +0000

    Gridview lista

--- E-Commerce/Admin/List.aspx.cs
+++ E-Commerce/Admin/List.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +14,40 @@ namespace E_Commerce.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+
+            if (!IsPostBack)
+            {
+
+                if (!IsPostBack)
+                {
+                    string connectionString = ConfigurationManager.ConnectionStrings["ConnectionStringDB"].ConnectionString.ToString();
+
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    
+                    SqlCommand cmd = new SqlCommand("Select * from Films;", connection);
+
+                    
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                    
+                    DataTable DB = new DataTable();
+
+                    
+                    adapter.Fill(DB);
+
+                    
+                    GridView1.DataSource = DB;
+                    GridView1.DataBind();
+
+                    
+                    connection.Close();
+                }
+
+
+            }
 
         }
     }
