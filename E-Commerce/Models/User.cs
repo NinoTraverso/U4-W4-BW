b2689c76ce22b88f770c6bd87c15f677f06e6ee0
Giuseppe Cifarelli <cifarelli1995@gmail.com>
2023-09-18 11:23:38 +0200

    Added Classes

---
+++ E-Commerce/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Image { get ; set; }
+
+        public User() { }
+        public User(string name, string surname, string username, string password, string role, string image)
+        {
+            Name = name;
+            Surname = surname;
+            Username = username;
+            Password = password;
+            Role = role;
+            Image = image;
+        }
+    }
+}
