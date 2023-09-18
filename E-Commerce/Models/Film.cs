7eb72792c4a1b3fc8ba90669656822821101f435
andreas ubertini <ubertiniandreas@gmail.com>
2023-09-18 11:05:43 +0200

    classes added

---
+++ E-Commerce/Models/Film.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce.Models
+{
+    public class Film
+    {
+        public string Title { get; set; }
+        public string Production { get; set; }
+        public string Category { get; set; }
+        public string Year { get; set; }
+        public string Duration { get; set; }
+        public string FirstActor { get; set; }
+        public double Price { get; set; }
+        public string BackgroundImg { get; set; }
+        public string CoverImg {  get; set; }
+        public string Img1 { get; set; }
+        public string Img2 { get; set; }
+        public string Img3 { get; set; }
+        public string director { get; set; }
+        public double Rating { get; set; }
+
+
+    }
+}
---
+++ E-Commerce/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce.Models
+{
+    public class User
+    {
+        
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string ProfileImg { get; set; }
+        public User() { }
+        public User(string name, string surname, string username, string email, string password, string role, string profileImg)
+        {
+            Name = name;
+            Surname = surname;
+            Username = username;
+            Email = email;
+            Password = password;
+            Role = role;
+            ProfileImg = profileImg;
+        }
+    }
+}
