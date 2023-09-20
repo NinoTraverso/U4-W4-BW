cf23a693034f255f15b23af782364a9db1e77be2
andreas ubertini <ubertiniandreas@gmail.com>
2023-09-20 11:48:18 +0200

    Update ModFilm.aspx.cs

--- E-Commerce/Admin/ModFilm.aspx.cs
+++ E-Commerce/Admin/ModFilm.aspx.cs
@@ -77,11 +77,16 @@ namespace E_Commerce.Admin
 
         protected void ModifyButton_Click(object sender, EventArgs e)
         {
+            Film film = DB.getFilmById(Convert.ToInt32(Request.QueryString["id"]));
             string fileNameBg = "";
             if (BackgroundUpload.HasFile)
             {
                 fileNameBg = BackgroundUpload.FileName;
                 BackgroundUpload.SaveAs(Server.MapPath($"/Content/assets/{BackgroundUpload.FileName}"));
+            }
+            else
+            {
+                fileNameBg = film.BackgroundImg;
             }
             string fileNameCover = "";
             if (CoverImgUpload.HasFile)
@@ -89,11 +94,19 @@ namespace E_Commerce.Admin
                 fileNameCover = CoverImgUpload.FileName;
                 CoverImgUpload.SaveAs(Server.MapPath($"/Content/assets/{CoverImgUpload.FileName}"));
             }
+            else
+            {
+                fileNameCover = film.CoverImg;
+            }
             string fileNameImg1 = "";
             if (Img1Upload.HasFile)
             {
                 fileNameImg1 = Img1Upload.FileName;
                 Img1Upload.SaveAs(Server.MapPath($"/Content/assets/{Img1Upload.FileName}"));
+            }
+            else
+            {
+                fileNameImg1 = film.Img1;
             }
             string fileNameImg2 = "";
             if (Img2Upload.HasFile)
@@ -101,11 +114,19 @@ namespace E_Commerce.Admin
                 fileNameImg2 = Img2Upload.FileName;
                 Img2Upload.SaveAs(Server.MapPath($"/Content/assets/{Img2Upload.FileName}"));
             }
+            else
+            {
+                fileNameImg2 = film.Img2;
+            }
             string fileNameImg3 = "";
             if (Img3Upload.HasFile)
             {
                 fileNameImg3 = Img3Upload.FileName;
                 Img3Upload.SaveAs(Server.MapPath($"/Content/assets/{Img3Upload.FileName}"));
+            }
+            else
+            {
+                fileNameImg3 = film.Img3;
             }
             int id= Convert.ToInt32(Request.QueryString["id"]);
             DB.Modify(id,TitleBox.Text, ProductionBox.Text, CategoryBox.Text, YearBox.Text, DurationBox.Text, FirstActorBox.Text, Convert.ToDouble(PriceBox.Text), fileNameBg, fileNameCover, fileNameImg1, fileNameImg2, fileNameImg3, DirectorBox.Text, Convert.ToDouble(RatingBox.Text));
